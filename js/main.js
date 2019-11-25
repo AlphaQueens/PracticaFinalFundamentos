d50d6b2a4a4e05d0b96455578e1e5be8c3353491
Coroko <45878223+Coroko@users.noreply.github.com>
2019-11-25 12:12:47 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
-document.getElementsByTagName("body")[0].appendChild(document.createElement("div").className("flex-container").appendChild(document.createElement("table").id("main_table")));
+body = document.getElementsByTagName("body")[0];
+body.appendChild(document.createElement("div").className("flex-container").appendChild(document.createElement("table").id("main_table")));
