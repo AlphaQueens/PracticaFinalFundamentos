6e2acc4ae4caf07ef374d04f1d9f3c8a75359afa
Coroko <45878223+Coroko@users.noreply.github.com>
2019-11-27 10:26:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { grados } from "grados.js";
+import { masters } from "masters.js";
 let body = document.getElementsByTagName('body')[0];
 let flex_container = document.createElement("div");
 flex_container.className = "flex-container";
@@ -8,11 +10,14 @@ table.id = "main_table";
 flex_container.appendChild(table);
 
 function pintar() {
+    var thead = document.createElement("thead");
+    table.appendChild();
+    var keys = Object.keys(grados).array.forEach(k => {
+        console.log(k);
+    });
 
-
-
-
-};
+}
+pintar();
 
 function ordenar(direccion, objeto_a_ordenar) {
 
