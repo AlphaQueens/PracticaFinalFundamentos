3cceeacddff92e54a586cf680e79e38cf446ad51
Coroko <45878223+Coroko@users.noreply.github.com>
2019-11-27 09:53:11 +0100

    cambios generales

--- js/main.js
+++ js/main.js
@@ -7,32 +7,11 @@ let table = document.createElement("table");
 table.id = "main_table";
 flex_container.appendChild(table);
 
-
-for (let i = 0;i < grados.length;i++)
-{
-  console.log(grados[i].centro);
-  console.log(grados[i].logo);
-  console.log(grados[i].correo);
-  console.log(grados[i].telefono);
-  console.log(grados[i].web);
-  console.log(grados[i].estudio);
-  console.log(grados[i].duracion);
-  console.log(grados[i].precio);
-}
+function pintar() {
 
 
-for (let i = 0;i < masters.length;i++)
-{
-  console.log(masters[i].centro);
-  console.log(masters[i].logo);
-  console.log(masters[i].correo);
-  console.log(masters[i].telefono);
-  console.log(masters[i].web);
-  console.log(masters[i].estudio);
-  console.log(masters[i].duracion);
-  console.log(masters[i].precio);
-}
 
-function pintar() {};
+
+};
 
 function ordenar(direccion, objeto_a_ordenar) {};
