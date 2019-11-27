4fbb2f83689792611b8a7a67c86561727713cf80
AlphaQueens <34938718+AlphaQueens@users.noreply.github.com>
2019-11-27 11:35:34 +0100

    Cositas con sentido ya

--- js/main.js
+++ js/main.js
@@ -1,8 +1,175 @@
 let body = document.getElementsByTagName('body')[0];
-let flex_container = document.createElement("div");
-flex_container.className = "flex-container";
-flex_container.id = "main_div";
-body.appendChild(flex_container);
-let table = document.createElement("table");
-table.id = "main_table";
-flex_container.appendChild(table);
+
+
+function crear_tabla () {
+  let flex_container = document.createElement("div");
+  flex_container.className = "flex-container";
+  flex_container.id = "main_div";
+  body.appendChild(flex_container);
+
+  let table_grados = document.createElement("table");
+  table_grados.id = "table_grados";
+  table_grados.setAttribute('border',1);
+  flex_container.appendChild(table_grados);
+
+  let table_masters = document.createElement("table");
+  table_masters.id = "table_masters";
+  table_masters.setAttribute('border',1);
+  flex_container.appendChild(table_masters);
+}
+
+function pintar_grados() {
+  let row_headers = document.createElement('tr');
+  table_grados.appendChild(row_headers);
+
+
+  let headerCentro = document.createElement('th');
+  let headerContacto = document.createElement('th');
+  let headerEstudio = document.createElement('th');
+  let headerDuracionPrecio = document.createElement('th');
+
+  headerCentro.innerHTML = 'CENTRO';
+  headerContacto.innerHTML = 'CONTACTO';
+  headerEstudio.innerHTML = 'GRADO';
+  headerDuracionPrecio.innerHTML = 'DURACIÓN Y PRECIO';
+
+  row_headers.appendChild(headerCentro);
+  row_headers.appendChild(headerContacto);
+  row_headers.appendChild(headerEstudio);
+  row_headers.appendChild(headerDuracionPrecio);
+
+  for (let i = 0;i < grados.length;i++)
+  {
+    let row = document.createElement('tr');
+    table_grados.appendChild(row);
+
+    var centroTd = document.createElement('td');
+    var contactoTd = document.createElement('td');
+    var gradoTd = document.createElement('td');
+    var duracionPrecioTd = document.createElement('td');
+
+    var centro = document.createElement('p');
+    centro.innerHTML = grados[i].centro;
+    centroTd.appendChild(centro);
+
+    var img = document.createElement('img');
+    img.setAttribute('src', grados[i].logo);
+    img.setAttribute('width', 150);
+    img.setAttribute('height', 150);
+    centroTd.appendChild(img);
+
+    var correo = document.createElement('p');
+    var a = document.createElement('a');
+    a.href = grados[i].correo;
+    a.innerHTML = "Aqui poner correo sin mailto";
+    correo.appendChild(a);
+    contactoTd.appendChild(correo);
+
+    var telefono = document.createElement('p');
+    telefono.innerHTML = grados[i].telefono;
+    contactoTd.appendChild(telefono);
+
+    var web = document.createElement('p');
+    var a = document.createElement('a');
+    a.href = grados[i].web;
+    a.innerHTML = 'Web';
+    web.appendChild(a);
+    contactoTd.appendChild(web);
+
+    gradoTd.innerHTML = grados[i].estudio;
+
+    var duracion = document.createElement('p');
+    duracion.innerHTML = grados[i].duracion;
+    duracionPrecioTd.appendChild(duracion);
+
+    var precio = document.createElement('p');
+    precio.innerHTML = grados[i].precio;
+    duracionPrecioTd.appendChild(precio);
+
+    row.appendChild(centroTd);
+    row.appendChild(contactoTd);
+    row.appendChild(gradoTd);
+    row.appendChild(duracionPrecioTd);
+  }
+};
+
+function pintar_masters() {
+
+  let row_headers = document.createElement('tr');
+  table_masters.appendChild(row_headers);
+
+  let headerCentro = document.createElement('th');
+  let headerContacto = document.createElement('th');
+  let headerEstudio = document.createElement('th');
+  let headerDuracionPrecio = document.createElement('th');
+
+  headerCentro.innerHTML = 'CENTRO';
+  headerContacto.innerHTML = 'CONTACTO';
+  headerEstudio.innerHTML = 'GRADO';
+  headerDuracionPrecio.innerHTML = 'DURACIÓN Y PRECIO';
+
+  row_headers.appendChild(headerCentro);
+  row_headers.appendChild(headerContacto);
+  row_headers.appendChild(headerEstudio);
+  row_headers.appendChild(headerDuracionPrecio);
+
+  for (let i = 0;i < masters.length;i++)
+  {
+    let row = document.createElement('tr');
+    table_masters.appendChild(row);
+
+    var centroTd = document.createElement('td');
+    var contactoTd = document.createElement('td');
+    var gradoTd = document.createElement('td');
+    var duracionPrecioTd = document.createElement('td');
+
+    var centro = document.createElement('p');
+    centro.innerHTML = masters[i].centro;
+    centroTd.appendChild(centro);
+
+    var img = document.createElement('img');
+    img.setAttribute('src', masters[i].logo);
+    img.setAttribute('width', 150);
+    img.setAttribute('height', 150);
+    centroTd.appendChild(img);
+
+    var correo = document.createElement('p');
+    var a = document.createElement('a');
+    a.href = masters[i].correo;
+    a.innerHTML = "Aqui poner correo sin mailto";
+    correo.appendChild(a);
+    contactoTd.appendChild(correo);
+
+    var telefono = document.createElement('p');
+    telefono.innerHTML = masters[i].telefono;
+    contactoTd.appendChild(telefono);
+
+    var web = document.createElement('p');
+    a = document.createElement('a');
+    a.href = masters[i].web;
+    a.innerHTML = 'Web';
+    web.appendChild(a);
+    contactoTd.appendChild(web);
+
+    gradoTd.innerHTML = masters[i].estudio;
+
+    var duracion = document.createElement('p');
+    duracion.innerHTML = masters[i].duracion;
+    duracionPrecioTd.appendChild(duracion);
+
+    var precio = document.createElement('p');
+    precio.innerHTML = masters[i].precio;
+    duracionPrecioTd.appendChild(precio);
+
+    row.appendChild(centroTd);
+    row.appendChild(contactoTd);
+    row.appendChild(gradoTd);
+    row.appendChild(duracionPrecioTd);
+  }
+};
+
+function ordenar(direccion, objeto_a_ordenar) {};
+
+crear_tabla();
+pintar_grados();
+pintar_masters();
--- js/masters.js
+++ js/masters.js
@@ -1,152 +1,152 @@
-[
-  { 
-    "centro": "Universidad Autónoma de Madrid",
+var masters = [
+  {
+    "centro": "Universidad AutÃ³noma de Madrid",
     "logo": "https://michollo.com/wp-content/uploads/2019/09/uam-1-1024x547.png",
-    "correo": "mailto:secretaria.bigdata@uam.es",    
+    "correo": "mailto:secretaria.bigdata@uam.es",
     "telefono": "914 973 931",
     "web": "https://www.uam.es/UAM/Home.htm?language=es",
     "estudio": "Master en Big Data y Data Science",
-    "duracion": "2 años de duración",
-    "precio": "5000€ (3 Becas disponibles)"
+    "duracion": "2 aÃ±os de duraciÃ³n",
+    "precio": "5.000â‚¬ (3 Becas disponibles)"
   },
-  { 
+  {
     "centro": "Universidad Complutense de Madrid",
     "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Escudo_de_la_Universidad_Complutense_de_Madrid.svg/1200px-Escudo_de_la_Universidad_Complutense_de_Madrid.svg.png",
     "correo": "mailto:info@masterbigdataucm.com",
     "telefono": "913 944 004",
     "web": "https://www.masterbigdataucm.com/",
     "estudio": "Master en Big Data y Business Analytics",
-    "duracion": "2 años de duración",
-    "precio": "6400€ - presencial | 4300€ - online (Becas disponibles)"
+    "duracion": "2 aÃ±os de duraciÃ³n",
+    "precio": "6.400â‚¬ - presencial | 4300â‚¬ - online (Becas disponibles)"
   },
-  { 
+  {
     "centro": "IMF Business School",
     "logo": "https://www.imf-formacion.com/img/logos/logo-imf-alta-cuadrado.jpg",
     "correo": "mailto:contacto@imf-formacion.com",
     "telefono": "900 318 111",
     "web": "https://www.imf-formacion.com/masters-profesionales/master-big-data-business-intelligence",
     "estudio": "Master en Big Data y Business Analytics",
-    "duracion": "1 año de duración",
-    "precio": "A consultar (Becas disponibles)"
+    "duracion": "1 aÃ±o de duraciÃ³n",
+    "precio": "17.000â‚¬ (Becas disponibles)"
   },
-  { 
-    "centro": "Universdad Europea",
-    "logo": "http://www.alemanista.com/wp-content/uploads/2013/01/Logo-UNED-verde.jpg",
+  {
+    "centro": "Universdad Europea de Madrid",
+    "logo": "https://pbs.twimg.com/profile_images/864871794892820480/rRNFy8nM.jpg",
     "correo": "mailto:dpo@universidadeuropea.es",
     "telefono": "917 407 272",
     "web": "https://universidadeuropea.es/online/titulacion/master-big-data",
     "estudio": "Master Online en Big Data Analytics",
-    "duracion": "10 meses de duración",
-    "precio": "6.090€ (Becas disponibles)"
+    "duracion": "10 meses de duraciÃ³n",
+    "precio": "6.090â‚¬ (Becas disponibles)"
   },
-  { 
+  {
     "centro": "UNIR Universidad en Internet",
     "logo": "https://www.unir.net/wp-content/uploads/2019/07/unir-LOGO.png",
     "correo": "mailto:info@unir.net",
     "telefono": "941 209 743",
     "web": "https://www.unir.net/ingenieria/master-visual-analytics-big-data/549200001429/",
     "estudio": "Master en Visual Analytics y Big Data",
-    "duracion": "1 año de duración",
+    "duracion": "1 aÃ±o de duraciÃ³n",
     "precio": "A consultar"
   },
-  { 
-    "centro": "UNED Universidad Nacional de Educación a Distancia",
-    "logo": "https://www.unir.net/wp-content/uploads/2019/07/unir-LOGO.png",
+  {
+    "centro": "UNED Universidad Nacional de EducaciÃ³n a Distancia",
+    "logo": "http://www.alemanista.com/wp-content/uploads/2013/01/Logo-UNED-verde.jpg",
     "correo": "mailto:infouned@adm.uned.es",
     "telefono": "913 989 025",
     "web": "https://www.masterbigdatauned.com/",
     "estudio": "Master en Big Data y Business Analytics",
-    "duracion": "10 meses de duración",
-    "precio": "4.080€"
+    "duracion": "10 meses de duraciÃ³n",
+    "precio": "4.080â‚¬"
   },
-  { 
+  {
     "centro": "MSMK Madrid School of Marketing",
     "logo": "http://www.claraavilac.com/wp-content/uploads/2015/09/logo-msmk.jpg",
     "correo": "mailto:info@msmk.eu",
     "telefono": "917 816 962",
     "web": "https://madridschoolofmarketing.es/masters-y-programas/master-executive-en-big-data-y-analytics",
     "estudio": "Master Executive en Big Data y Analytics",
-    "duracion": "5 meses de duración",
+    "duracion": "5 meses de duraciÃ³n",
     "precio": "A consultar"
   },
-  { 
+  {
     "centro": "n+3 business school",
     "logo": "https://www.emagister.com/es/albums/1/1/2/3/0/xl_cropped_312651ef22b3564a5e325fe715de9e77.jpg",
     "correo": "mailto:info@negociosyestrategia.com",
     "telefono": "911 117 619",
     "web": "https://negociosyestrategia.com/master/big-data-business-analytics/",
     "estudio": "Master en Big Data y Business Analytics",
-    "duracion": "10 meses de duración",
+    "duracion": "10 meses de duraciÃ³n",
     "precio": "A consultar (Becas disponibles)"
   },
-  { 
+  {
     "centro": "INESEM Business school",
     "logo": "https://www.estudiaradistancia.es/logos/original/logo-inesem-business-school.png",
     "correo": "mailto:info@inesem.com",
     "telefono": "958 050 205",
     "web": "https://www.inesem.es/Master-En-Big-Data",
     "estudio": "Master en Business Intelligence y Big Data",
-    "duracion": "10 meses de duración",
-    "precio": "2000€ (Becas disponibles)"
+    "duracion": "10 meses de duraciÃ³n",
+    "precio": "2.000â‚¬ (Becas disponibles)"
   },
-  { 
+  {
     "centro": "Spain Business School",
     "logo": "https://pbs.twimg.com/profile_images/937658184529055744/J0w16gCb.jpg",
     "correo": "mailto:info@spainbs.com",
     "telefono": "917 191 000",
     "web": "https://www.spainbs.com/master-analitica-web-big-data",
-    "estudio": "Master en Analítica Web y Big Data",
-    "duracion": "1 año de duración",
-    "precio": "3.500€ (Becas disponibles)"
+    "estudio": "Master en AnalÃ­tica Web y Big Data",
+    "duracion": "1 aÃ±o de duraciÃ³n",
+    "precio": "3.500â‚¬ (Becas disponibles)"
   },
-  { 
+  {
     "centro": "MBIT School Business Data Intelligence",
     "logo": "https://static.wixstatic.com/media/6fe95b_b611761ff1c94e4cae552f85ff75fbb9.png/v1/fit/w_2500,h_1330,al_c/6fe95b_b611761ff1c94e4cae552f85ff75fbb9.png",
     "correo": "mailto:info@mbitschool.com",
     "telefono": "915 048 600",
     "web": "https://www.mbitschool.com/master-big-data-cloud",
     "estudio": "Master Executive en Big Data, Cloud y Analytics",
-    "duracion": "1 año de duración",
+    "duracion": "1 aÃ±o de duraciÃ³n",
     "precio": "A consultar"
   },
-  { 
+  {
     "centro": "Universidad de Navarra",
     "logo": "https://centrorenzulli.es/renzulliwp/wp-content/uploads/2015/05/UNAV-LOGO-2.png",
     "correo": "mailto:bigdata@unav.es",
     "telefono": "914 514 341",
     "web": "https://www.unav.edu/web/master-en-big-data-science",
     "estudio": "Master Executive en Big Data Science (MEBDS)",
-    "duracion": "1 año de duración",
+    "duracion": "1 aÃ±o de duraciÃ³n",
     "precio": "A consultar"
   },
-  { 
+  {
     "centro": "BTS Barcelona Technology School",
     "logo": "https://4d17d13o8yuu3wluzr4c2feo-wpengine.netdna-ssl.com/wp-content/uploads/2019/02/btslogo.png",
     "correo": "mailto:info@barcelonatechnologyschool.com",
     "telefono": "933 041 721",
     "web": "https://barcelonatechnologyschool.com/master/master-in-big-data-solutions/",
     "estudio": "Master en Big Data Solutions (MBDS)",
-    "duracion": "9 meses de duración",
-    "precio": "17.000€"
+    "duracion": "9 meses de duraciÃ³n",
+    "precio": "17.000â‚¬"
   },
-  { 
+  {
     "centro": "EAE Business School",
     "logo": "https://www.vectorlogo.es/wp-content/uploads/2017/02/logo-vector-eae-business-school.jpg",
     "correo": "mailto:info@eae.com",
     "telefono": "900 494 805",
     "web": "https://www.eae.es/full-time/master-big-data-and-analytics/presentacion",
     "estudio": "Master en Big Data y Analytics",
-    "duracion": "1 año de duración",
-    "precio": "14.000€"
+    "duracion": "1 aÃ±o de duraciÃ³n",
+    "precio": "14.000â‚¬"
   },
-  { 
+  {
     "centro": "U-tad",
     "logo": "https://pbs.twimg.com/profile_images/994861193964728320/AY3I-Ow0_400x400.jpg",
     "correo": "mailto:info@u-tad.com",
     "telefono": "900 373 379",
     "web": "https://www.u-tad.com/estudios/master-en-data-science-big-data/",
     "estudio": "Master en Data Science y Big Data",
-    "duracion": "2 años de duración",
+    "duracion": "2 aÃ±os de duraciÃ³n",
     "precio": "A consultar"
   }
-]
+];
