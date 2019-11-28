f8f8096d910a1c77619a99a4ea6c7e10de9726e1
Coroko <45878223+Coroko@users.noreply.github.com>
2019-11-28 20:30:42 +0100

    Update table_grados.js

--- js/table_grados.js
+++ js/table_grados.js
@@ -1,5 +1,6 @@
 crear_tabla();
 pintar(grados);
+
 headerCentro = document.getElementById("centro");
 headerEstudio = document.getElementById("grado");
 headerDuracionPrecio = document.getElementById("precio");
@@ -7,4 +8,31 @@ headerDuracionPrecio = document.getElementById("precio");
 headerCentro.addEventListener("click", e => (ordenar(1, headerCentro.innerHTML)));
 headerEstudio.addEventListener("click", e => (ordenar(1, headerEstudio.innerHTML)));
 headerDuracionPrecio.addEventListener("click", e => (ordenar(1, headerDuracionPrecio.innerHTML)));
+
+headerCentro.addEventListener("mouseenter", e => {
+
+    document.getElementById("centro").style.backgroundColor = "#366379";
+});
+headerEstudio.addEventListener("mouseenter", e => {
+
+    document.getElementById("grado").style.backgroundColor = "#366379";
+});
+headerDuracionPrecio.addEventListener("mouseenter", e => {
+
+    document.getElementById("precio").style.backgroundColor = "#366379";
+});
+
+headerCentro.addEventListener("mouseleave", e => {
+
+    document.getElementById("centro").style.backgroundColor = "#6DC7F3";
+});
+headerEstudio.addEventListener("mouseleave", e => {
+
+    document.getElementById("grado").style.backgroundColor = "#6DC7F3";
+});
+headerDuracionPrecio.addEventListener("mouseleave", e => {
+
+    document.getElementById("precio").style.backgroundColor = "#6DC7F3";
+});
+
 window.onscroll = function() { scrollfunction() };
