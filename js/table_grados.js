ddcc72cbb640d8a5087adedfb0f38c6923e37873
Coroko <45878223+Coroko@users.noreply.github.com>
2019-11-28 16:43:54 +0100

    q

--- js/table_grados.js
+++ js/table_grados.js
@@ -1,3 +1,10 @@
 crear_tabla();
 pintar_grados();
 window.onscroll = function() { scrollfunction() };
+document.getElementsByTagName("h1")[0].addEventListener("mouseenter", function() {
+    document.getElementById("navbar").style.top = "0";
+
+});
+document.getElementsByTagName("h1")[0].addEventListener("mouseleave", function() {
+    document.getElementById("navbar").style.top = "-50";
+})
--- js/table_master.js
+++ js/table_master.js
@@ -1,3 +1,10 @@
 crear_tabla();
 pintar_masters();
 window.onscroll = function() { scrollfunction() };
+document.getElementsByTagName("h1")[0].addEventListener("mouseenter", function() {
+    document.getElementById("navbar").style.top = "0";
+
+});
+document.getElementsByTagName("h1")[0].addEventListener("mouseleave", function() {
+    document.getElementById("navbar").style.top = "-50";
+})
