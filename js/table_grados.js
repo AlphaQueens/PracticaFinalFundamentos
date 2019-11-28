8ec5523b3544784d684fc194b9c30b6f1bc95183
Coroko <45878223+Coroko@users.noreply.github.com>
2019-11-28 16:57:46 +0100

    qqq

--- js/table_grados.js
+++ js/table_grados.js
@@ -1,12 +1,3 @@
 crear_tabla();
 pintar_grados();
 window.onscroll = function() { scrollfunction() };
-document.getElementsByTagName("h1")[0].addEventListener("mouseenter", e => {
-    document.getElementById("navbar").style.top = "0px";
-    console.log("yosta es un perro");
-
-});
-document.getElementsByTagName("h1")[0].addEventListener("mouseleave", e => {
-    document.getElementById("navbar").style.top = "-50px";
-    console.log("yosta es un puto");
-})
--- js/table_master.js
+++ js/table_master.js
@@ -1,10 +1,3 @@
 crear_tabla();
 pintar_masters();
 window.onscroll = function() { scrollfunction() };
-document.getElementsByTagName("h1")[0].addEventListener("mouseenter", function() {
-    document.getElementById("navbar").style.top = "0";
-
-});
-document.getElementsByTagName("h1")[0].addEventListener("mouseleave", function() {
-    document.getElementById("navbar").style.top = "-50";
-})
