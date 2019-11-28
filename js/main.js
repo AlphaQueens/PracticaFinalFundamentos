315d12e7f47aa31e9ae81a68cfe258888cc61b0f
Coroko <45878223+Coroko@users.noreply.github.com>
2019-11-28 19:05:32 +0100

    he terminado algo
    El sort ya se como conseguir todas los nombres de los td, el menu esta completo, falta la pagina principal, y dejarlo bonito.

--- js/main.js
+++ js/main.js
@@ -7,15 +7,10 @@ function crear_tabla() {
     flex_container.clasName = "flex-container";
     body.appendChild(flex_container);
 
-    let table_grados = document.createElement("table");
-    table_grados.id = "table_grados";
-    table_grados.setAttribute('border', 1);
-    flex_container.appendChild(table_grados);
-
-    let table_masters = document.createElement("table");
-    table_masters.id = "table_masters";
-    table_masters.setAttribute('border', 1);
-    flex_container.appendChild(table_masters);
+    let table = document.createElement("table");
+    table.id = "table";
+    table.setAttribute('border', 1);
+    flex_container.appendChild(table);
 }
 
 function pintar_headers() {
@@ -36,7 +31,10 @@ function pintar_headers() {
     headerEstudio.innerHTML = 'GRADO';
     headerDuracionPrecio.innerHTML = 'DURACIÓN Y PRECIO';
 
-    //headerCentro.addEventListener()
+    headerCentro.id = "centro";
+    headerContacto.id = "contacto";
+    headerEstudio.id = "grado";
+    headerDuracionPrecio.id = "precio";
 
     row_headers.appendChild(headerCentro);
     row_headers.appendChild(headerContacto);
@@ -45,126 +43,108 @@ function pintar_headers() {
     return row_headers;
 }
 
-function pintar_grados() {
-    var table_grados = document.getElementById("table_grados");
-    table_grados.appendChild(pintar_headers());
-    for (let i = 0; i < grados.length; i++) {
-        let row = document.createElement('tr');
-        table_grados.appendChild(row);
-
-
-        var centroTd = document.createElement('td');
-        var contactoTd = document.createElement('td');
-        var gradoTd = document.createElement('td');
-        var duracionPrecioTd = document.createElement('td');
-
-        var centro = document.createElement('p');
-        centro.innerHTML = grados[i].centro;
-        centroTd.appendChild(centro);
-
-        var img = document.createElement('img');
-        img.setAttribute('src', grados[i].logo);
-        img.setAttribute('width', 150);
-        img.setAttribute('height', 150);
-        centroTd.appendChild(img);
-
-        var correo = document.createElement('p');
-        var a = document.createElement('a');
-        a.href = "mailto:" + grados[i].correo;
-        a.innerHTML = grados[i].correo;
-        correo.appendChild(a);
-        contactoTd.appendChild(correo);
-
-        var telefono = document.createElement('p');
-        telefono.innerHTML = grados[i].telefono;
-        contactoTd.appendChild(telefono);
-
-        var web = document.createElement('p');
-        var a = document.createElement('a');
-        a.href = grados[i].web;
-        a.innerHTML = 'Web';
-        web.appendChild(a);
-        contactoTd.appendChild(web);
-
-        gradoTd.innerHTML = grados[i].estudio;
-
-        var duracion = document.createElement('p');
-        duracion.innerHTML = grados[i].duracion;
-        duracionPrecioTd.appendChild(duracion);
-
-        var precio = document.createElement('p');
-        precio.innerHTML = grados[i].precio;
-        duracionPrecioTd.appendChild(precio);
-
-        row.appendChild(centroTd);
-        row.appendChild(contactoTd);
-        row.appendChild(gradoTd);
-        row.appendChild(duracionPrecioTd);
+function pintar(obj) {
+    var tables = document.getElementById("table");
+    tables.appendChild(pintar_headers());
+    for (let i = 0; i < obj.length; i++) {
+        pintartd(tables, obj, i);
     }
 };
 
-function pintar_masters() {
-    var table_master = document.getElementById("table_grados");
-    table_master.appendChild(pintar_headers());
-    for (let i = 0; i < masters.length; i++) {
-        let row = document.createElement('tr');
-        table_masters.appendChild(row);
+function pintartd(table, obj, i) {
+    let row = document.createElement('tr');
+    table.appendChild(row);
 
-        var centroTd = document.createElement('td');
-        var contactoTd = document.createElement('td');
-        var gradoTd = document.createElement('td');
-        var duracionPrecioTd = document.createElement('td');
+    var centroTd = document.createElement('td');
+    var contactoTd = document.createElement('td');
+    var gradoTd = document.createElement('td');
+    var duracionPrecioTd = document.createElement('td');
 
-        var centro = document.createElement('p');
-        centro.innerHTML = masters[i].centro;
-        centroTd.appendChild(centro);
+    centroTd.className = obj[i].centro;
+    contactoTd.className = obj[i].telefono;
+    gradoTd.className = obj[i].grado;
+    duracionPrecioTd.className = obj[i].precio;
 
-        var img = document.createElement('img');
-        img.setAttribute('src', masters[i].logo);
-        img.setAttribute('width', 150);
-        img.setAttribute('height', 150);
-        centroTd.appendChild(img);
 
-        var correo = document.createElement('p');
-        var a = document.createElement('a');
-        a.href = "mailto:" + masters[i].correo;
-        a.innerHTML = masters[i].correo;
-        correo.appendChild(a);
-        contactoTd.appendChild(correo);
+    var centro = document.createElement('p');
+    centro.innerHTML = obj[i].centro;
+    centroTd.appendChild(centro);
 
-        var telefono = document.createElement('p');
-        telefono.innerHTML = masters[i].telefono;
-        contactoTd.appendChild(telefono);
+    var img = document.createElement('img');
+    img.setAttribute('src', obj[i].logo);
+    img.setAttribute('width', 150);
+    img.setAttribute('height', 150);
+    centroTd.appendChild(img);
 
-        var web = document.createElement('p');
-        a = document.createElement('a');
-        a.href = masters[i].web;
-        a.innerHTML = 'Web';
-        web.appendChild(a);
-        contactoTd.appendChild(web);
+    var correo = document.createElement('p');
+    var a = document.createElement('a');
+    a.href = "mailto:" + obj[i].correo;
+    a.innerHTML = obj[i].correo;
+    correo.appendChild(a);
+    contactoTd.appendChild(correo);
 
-        gradoTd.innerHTML = masters[i].estudio;
+    var telefono = document.createElement('p');
+    telefono.innerHTML = obj[i].telefono;
+    contactoTd.appendChild(telefono);
 
-        var duracion = document.createElement('p');
-        duracion.innerHTML = masters[i].duracion;
-        duracionPrecioTd.appendChild(duracion);
+    var web = document.createElement('p');
+    a = document.createElement('a');
+    a.href = obj[i].web;
+    a.innerHTML = 'Web';
+    web.appendChild(a);
+    contactoTd.appendChild(web);
 
-        var precio = document.createElement('p');
-        precio.innerHTML = masters[i].precio;
-        duracionPrecioTd.appendChild(precio);
+    gradoTd.innerHTML = obj[i].estudio;
 
-        row.appendChild(centroTd);
-        row.appendChild(contactoTd);
-        row.appendChild(gradoTd);
-        row.appendChild(duracionPrecioTd);
+    var duracion = document.createElement('p');
+    duracion.innerHTML = obj[i].duracion;
+    duracionPrecioTd.appendChild(duracion);
+
+    var precio = document.createElement('p');
+    precio.innerHTML = obj[i].precio;
+    duracionPrecioTd.appendChild(precio);
+
+    centroTd.id = "centro" + i;
+    contactoTd.id = "contacto" + i;
+    gradoTd.id = "grado" + i;
+    duracionPrecioTd.id = "precio" + i;
+
+    row.appendChild(centroTd);
+    row.appendChild(contactoTd);
+    row.appendChild(gradoTd);
+    row.appendChild(duracionPrecioTd);
+}
+
+function ordenar(dir, selected_thing) {
+    console.log(selected_thing);
+    var table = document.getElementById("table");
+    var tr = document.getElementsByTagName("tr");
+
+    var thingis = [];
+    switch (selected_thing) {
+        case "CENTRO":
+            for (var i = 0; i < tr.length - 1; i++) {
+                thingis.push(document.getElementById("centro" + i).children);
+                console.log(thingis[i][0]); //esto recoge el p de cada uno de los td del html
+            }
+            break;
+        case "CONTACTO":
+
+            break;
+        case "GRADO":
+
+            break;
+        case "DURACIÓN Y PRECIO":
+
+            break;
+        default:
+            break;
     }
 }
 
-function ordenar() {
 
 
 
-}
 window.onscroll = function() { scrollfunction() };
 
 function scrollfunction() {
@@ -176,19 +156,14 @@ function scrollfunction() {
 }
 document.getElementById('info_div').addEventListener("mouseenter", e => {
     document.getElementById("navbar").style.top = "0px";
-    console.log("yosta es un perro");
-
+    console.log("xd");
 });
 document.getElementById('info_div').addEventListener("mouseleave", e => {
     document.getElementById("navbar").style.top = "-50px";
-    console.log("yosta es un puto");
 });
 document.getElementById('navbar').addEventListener("mouseenter", e => {
     document.getElementById("navbar").style.top = "0px";
-    console.log("yosta es un perro");
-
 });
 document.getElementById('navbar').addEventListener("mouseleave", e => {
     document.getElementById("navbar").style.top = "-50px";
-    console.log("yosta es un puto");
 });
