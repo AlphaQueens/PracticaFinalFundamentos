42d8ef3fdf24479852a6506ec7203fb293a748fd
Coroko <45878223+Coroko@users.noreply.github.com>
2019-11-28 16:39:46 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -174,7 +174,7 @@ function scrollfunction() {
         document.getElementById("navbar").style.top = "-50px";
     }
 }
-document.getElementsByTagName("h1")[0].addEventListener("mouseenter", function() {
+document.getElementsByTagName("h1")[0].addEventListener("mousemove", function() {
     document.getElementById("navbar").style.top = "0";
 });
 document.getElementsByTagName("h1")[0].addEventListener("mouseleave", function() {
