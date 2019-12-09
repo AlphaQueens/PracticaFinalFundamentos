5a308c14b0105add9982204b2253409d56095903
Coroko <45878223+Coroko@users.noreply.github.com>
2019-12-09 12:43:20 +0100

    Update table_master.js

--- js/table_master.js
+++ js/table_master.js
@@ -8,4 +8,30 @@ headerDuracionPrecio = document.getElementById("precio");
 headerCentro.addEventListener("click", e => (ordenar(1, headerCentro.innerHTML)));
 headerEstudio.addEventListener("click", e => (ordenar(1, headerEstudio.innerHTML)));
 headerDuracionPrecio.addEventListener("click", e => (ordenar(1, headerDuracionPrecio.innerHTML)));
+headerCentro.addEventListener("mouseenter", e => {
+
+    document.getElementById("centro").style.backgroundColor = "#728cb2";
+});
+headerEstudio.addEventListener("mouseenter", e => {
+
+    document.getElementById("grado").style.backgroundColor = "#728cb2";
+});
+headerDuracionPrecio.addEventListener("mouseenter", e => {
+
+    document.getElementById("precio").style.backgroundColor = "#728cb2";
+});
+
+headerCentro.addEventListener("mouseleave", e => {
+
+    document.getElementById("centro").style.backgroundColor = "#2b2d2e";
+});
+headerEstudio.addEventListener("mouseleave", e => {
+
+    document.getElementById("grado").style.backgroundColor = "#2b2d2e";
+});
+headerDuracionPrecio.addEventListener("mouseleave", e => {
+
+    document.getElementById("precio").style.backgroundColor = "#2b2d2e";
+});
+
 window.onscroll = function() { scrollfunction() };
